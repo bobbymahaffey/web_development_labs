9431388d0af0a151cc2ac730bd847def4a3ddb58
Bobby Mahaffey <bobby.mahaffey@oit.edu>
2021-03-13 12:37:22 -0800

    lab6 Exercise 3

---
+++ Weeks/Week6/webapp/src/components/Instructors.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <h2 class='section-heading'>Instructors</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>Last Name</th>
+                    <th>First Name</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="instructors in Instructors" v-bind:key="instructors.lastName">
+                    <td>{{ instructors.lastName }}</td>
+                    <td>{{ instructors.firstName }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Tables',
+        mounted() {
+            this.Instructors = getInstructors();
+        },
+        data () {
+            return {
+                Instructors: []
+            }
+        }
+    }
+    function getInstructors() {
+        return JSON.parse(Instructors);
+    }
+    var Instructors = '[{"firstName": "Kameron", "lastName": "Mahaffey"}, {"firstName": "Taye", "lastName": "Mahaffey"}]';
+</script>
+
+<style scoped>
+    table, th, td {
+        padding: 10px;
+        border: 5px solid black; 
+        border-collapse: collapse;
+    }
+</style>
